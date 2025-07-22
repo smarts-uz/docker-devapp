6113ae91c4212fec6743d45b27db2e4d4a14b4ac
alpha20060309 <komoliddin487@gmail.com>
2025-07-22 14:07:09 +0500

    Change postgres image. Create and activate extensions

--- App/Ubuntu/installs.sh
+++ App/Ubuntu/installs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install
-
+apt-get update
 apt-get -y install wget net-tools mc
 apt-get -y install htop zip unzip p7zip-full p7zip-rar
 apt-get -y install systemd git nmap
 apt-get -y install apt-file lvm2 telnetd
-apt-get -y install python3 python3-pip busybox 
+apt-get -y install python3 python3-pip busybox
--- postgres/installs/configs.sh
+++ postgres/installs/configs.sh
@@ -1,3 +1,11 @@
 #!/sbin/openrc-run
 
 echo 'host all all 0.0.0.0/0 trust' >> /var/lib/postgresql/data/pg_hba.conf
+
+
+apt-get update && apt-get install -y \
+    postgresql-17-cron \
+    postgresql-17-http \
+    postgresql-17-pgaudit \
+    postgresql-contrib && \
+    rm -rf /var/lib/apt/lists/*
