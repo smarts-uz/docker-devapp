6a5e8cf6624259e0c1c1b697aae4b4b37921ccd8
Kamoliddin Fazliddinov <komoliddin487@gmail.com>
2025-09-09 11:08:40 +0500

    Enable schema loading

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,32 @@
+
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE EXTENSION IF NOT EXISTS pg_cron;
 CREATE EXTENSION IF NOT EXISTS pgaudit;
 CREATE EXTENSION IF NOT EXISTS http;
+
+-- Create an event trigger function
+CREATE OR REPLACE FUNCTION pgrst_watch() RETURNS event_trigger
+  LANGUAGE plpgsql
+  AS $$
+BEGIN
+  NOTIFY pgrst, 'reload schema';
+END;
+$$;
+
+-- This event trigger will fire after every ddl_command_end event
+CREATE EVENT TRIGGER pgrst_watch
+  ON ddl_command_end
+  EXECUTE PROCEDURE pgrst_watch();
+
+-- 1. Create the anon role (no login)
+CREATE ROLE anon NOLOGIN;
+
+-- 2. Grant anon permission to read all existing tables in public
+-- This must be done as a superuser or table owner
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO anon;
+
+-- 3. Ensure anon can read tables created in the future
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT ON TABLES TO anon;
