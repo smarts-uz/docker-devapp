46700a65890e3107c61855302e8ebf94edf73345
mukhtarovich-dev <dilbekk3@gmail.com>
2025-09-24 00:41:49 +0500

    Updated Dockerfile and configs.sh files: added new commands to install required PostgreSQL packages and removed old installation commands.

--- postgres/installs/configs.sh
+++ postgres/installs/configs.sh
@@ -1,9 +1,3 @@
 #!/sbin/openrc-run
 
 echo 'host all all 0.0.0.0/0 trust' >> /var/lib/postgresql/data/pg_hba.conf
-
-apt-get -yqq update
-apt-get -y install postgresql-17-cron
-apt-get -y install postgresql-17-http 
-apt-get -y install postgresql-17-pgaudit 
-apt-get -y install postgresql-contrib
