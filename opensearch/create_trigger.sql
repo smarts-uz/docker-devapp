5aa47e1d61f41f9e163650d72c7cd211e3fd94f7
Kamoliddin Fazliddinov <komoliddin487@gmail.com>
2025-11-24 14:22:24 +0500

    add also after insert

--- opensearch/create_trigger.sql
+++ opensearch/create_trigger.sql
@@ -1,35 +1,35 @@
 -- PLAYER
 CREATE TRIGGER notify__opensearch_player
-BEFORE UPDATE ON player
+BEFORE INSERT OR UPDATE ON player
 FOR EACH ROW
 EXECUTE FUNCTION notify__opensearch();
 
 -- CLUB
 CREATE TRIGGER notify__opensearch_club
-BEFORE UPDATE ON club
+BEFORE INSERT OR UPDATE ON club
 FOR EACH ROW
 EXECUTE FUNCTION notify__opensearch();
 
 -- TEAM
 CREATE TRIGGER notify__opensearch_team
-BEFORE UPDATE ON team
+BEFORE INSERT OR UPDATE ON team
 FOR EACH ROW
 EXECUTE FUNCTION notify__opensearch();
 
 -- MATCH
 CREATE TRIGGER notify__opensearch_match
-BEFORE UPDATE ON match
+BEFORE INSERT OR UPDATE ON match
 FOR EACH ROW
 EXECUTE FUNCTION notify__opensearch();
 
 -- PLAYER_RESULT
 CREATE TRIGGER notify__opensearch_player_result
-BEFORE UPDATE ON player_result
+BEFORE INSERT OR UPDATE ON player_result
 FOR EACH ROW
 EXECUTE FUNCTION notify__opensearch();
 
 -- PLAYER_POINT
 CREATE TRIGGER notify__opensearch_player_point
-BEFORE UPDATE ON player_point
+BEFORE INSERT OR UPDATE ON player_point
 FOR EACH ROW
 EXECUTE FUNCTION notify__opensearch();
