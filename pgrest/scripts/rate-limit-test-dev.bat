afa046ed1f30c9304619e38df82cf849a99c631c
Ramzbek Kenjayev <rkenjaev1@gmail.com>
2025-12-29 14:49:30 +0500

    Rate limiting pgrest servers

---
+++ pgrest/scripts/rate-limit-test-dev.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Set URL and token
+set "URL=https://ssl.smarts.uz/tour_team?select=*&limit=1"
+set "AUTH=Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1MjUyNSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJleHAiOjE3NzIwMDIyMzQsImlhdCI6MTc2NjgxODIzNH0.lpnlnmk4kWXGUGmbpFLTQtBFWbMuE4BK4Zn1pPY2J8Q"
+
+:: Loop 100 times
+for /l %%i in (1,1,100) do (
+    echo Request #%%i
+    curl --silent --location "!URL!" --header "Authorization: !AUTH!" --header "Prefer: count=estimated"
+    echo.
+    echo ---------------------------
+)
+
+pause
